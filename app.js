becb3b3f4a2a23647983c015eec3d7cf3edcc822
Kalan Prudhomme <cleaver9@gmail.com>
2021-03-18 11:02:39 -0700

    working bonus features

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const input = document.getElementById('change-name');
 const newName = document.getElementById('input-name');
+const sideName = document.getElementById('input-name-side');
 const button = document.getElementById('name-button');
-
+const background = document.getElementById('background');
+const clickValue = document.getElementById('name-number');
 console.log(input);
 
+let clickNumber = 0;
 
 button.addEventListener('click', () => {
+    clickNumber++;
     const userTyped = input.value;
+    sideName.textContent = userTyped;
     newName.textContent = userTyped;
+    setBg();
+    console.log(clickNumber);
+    clickValue.textContent = clickNumber;
+});
+
+//Found online, studying how to write from scratch
+const setBg = () => {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.background = "#" + randomColor;
+    let background = "#" + randomColor;
 }
+
